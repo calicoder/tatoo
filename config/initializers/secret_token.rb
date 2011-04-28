2dfea97600c99edf59ca57e1f6fd580a4dc59bd3
Andy Shin <andrewkshin@gmail.com>
2011-04-27 22:57:51 -0700

    rails is in

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tatoo::Application.config.secret_token = 'e94aab54128d75a7f78a60073262b9e6a1e5fe331e56316e481ae94abf004c32911af4f964a5a1f530db4f6c0857441279bfa8de66dfa2ebcfd7754b06f62320'
