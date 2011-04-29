0f35460b2a097911a476d43eb48044c25cb3d76e
Andy Shin <andrewkshin@gmail.com>
2011-04-29 13:09:33 -0700

    add tatoo specific configs

--- config/initializers/app_configuration.rb
+++ config/initializers/app_configuration.rb
@@ -1,4 +1,3 @@
-#Spree::Config.set(:admin_interface_logo => "/images/layout/headerlogo.png")
-#Spree::Config.set(:logo => "/images/layout/headerlogo.png")
-#Spree::Config.set(:site_name => "Horiyoshi The Third")
-#
+Spree::Config.set(:admin_interface_logo => "/images/layout/headerlogo.png")
+Spree::Config.set(:logo => "/images/layout/headerlogo.png")
+Spree::Config.set(:site_name => "Horiyoshi The Third")
